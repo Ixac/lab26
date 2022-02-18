98431af0fd84e1ff2020203b94ce15193fba7787
Ixac <ixacarrrows@gmail.com>
2022-02-18 16:43:00 +0700

    lab26 1

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -64,3 +64,48 @@ void List::append(int d){
 }
 
 //Write List::remove() here
+void List::remove(int idx)
+{
+	Node *current = root ;
+    Node *p = NULL ;
+
+    if(idx == 0)root = current->next ;
+    else
+    {
+        for(int i = 0 ; i < idx ; i++){
+            if(i == idx-1)
+            p = current ;
+            current = current->next;
+        }
+        p->next = current->next;
+    }
+    delete current ;
+
+}
+
+int main(){
+	List myList = {0,0};
+myList.append(5);
+myList.append(7);
+myList.append(11);
+myList.append(4);
+myList.append(12);
+myList.append(45);
+
+myList.insert(29,2);
+myList.insert(33,3);
+myList.insert(77,5);
+myList.insert(69,0);
+myList.show();
+cout << "\n"; 
+
+myList.remove(2);
+myList.show();
+cout << "\n"; 
+myList.remove(4);
+myList.show();
+cout << "\n";
+myList.remove(0);
+myList.show();
+cout << "\n";
+}
